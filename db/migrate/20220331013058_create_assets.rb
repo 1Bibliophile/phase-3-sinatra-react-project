24da577b395fafd456b83b277c83918393248281
David Morgenstern <DavidtheConfident@gmail.com>
2022-03-30 20:48:03 -0500

    Creates Table

--- db/migrate/20220331013058_create_assets.rb
+++ db/migrate/20220331013058_create_assets.rb
@@ -1,4 +1,17 @@
 class CreateAssets < ActiveRecord::Migration[6.1]
   def change
+    create_table :assets do |t|
+      t.string :name,
+      t.integer :date,
+      t.timestamps
   end
 end
+
+# Methods: # add_column, # add_index, # change_column, 
+           # change_table, #create_table, #drop_table,
+           # remove_column, # remove_index, #rename_column
+
+# Supported Column Types: :binary, :boolean, :date, :datetime,
+#                         :decimal, :float, :integer, :primary_key,
+#                         :string, :text, :time, :timestamps
+#
