79a416f8945ce5553c9616a3a428eaca4f8e0a47
David Morgenstern <DavidtheConfident@gmail.com>
2022-04-13 17:10:02 -0500

    Test 2.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 fx = Asset.create(name: "FX Networks", date: "1 June 1994")
 disney_brand = Asset.create(name: "Disney Branded Television", date: "15 July 1982")
 nat_geo = Asset.create(name: "National Geographic Global Networks", date: "1 September 1997")
-abc_news = Asset.create(name: "ABC News", date: '15 June 1945')
+abc_news = Asset.create(name: "ABC News", date: "15 June 1945")
 walt_disney_tv = Asset.create(name: "Walt Disney Television", date: "18 March 1985")
 
 Subsidiary.create(name: "FX", date: "1 June 1994", asset: fx)
